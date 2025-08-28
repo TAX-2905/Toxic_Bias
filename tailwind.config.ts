0bea2372c5e818f4436ce60e54c995bb10554bb8
lomesh takoory <lomesh.takoory1@umail.uom.ac.mu>
2025-08-28 20:03:27 +0400

    DARKREMOVED

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "tailwindcss";
 import typography from "@tailwindcss/typography";
 
 const config: Config = {
-  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
